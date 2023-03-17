a14e49a7223f1784cfeaf98b437a936bf1a6a336
Joseph Kishore <jeevankishore544@gmail.com>
2023-03-17 21:14:16 +0530

    added utils

--- configs/paths.py
+++ configs/paths.py
@@ -1,8 +1,15 @@
 MODEL_PATHS = {
-    'text_encoder' : '/models/trained_models/text_encoder.pt',
+    'text_encoder' : 'models/trained_models/text_encoder.pt',
     'latent_code_decoder' : 'models/trained_models/latent_code_decoder.pt',
-    'sbert' : '/models/SBERT',
-    'inversion' : '',
-    'stylegan2' : '',
-    'direction_classifier' : ''
+    'sbert' : 'models/trained_models/SBERT',
+    'inversion' : 'models/trained_models/psp_ffhq_encode.pt',
+    'stylegan2' : 'models/trained_models/psp_ffhq_encode.pt',
+    'direction_classifier' : 'models/trained_models/direction_classifier.pt'
 }
+
+def get_path(key) :
+    try :
+        path = MODEL_PATHS[key]
+    except Exception as e :
+        print(e)
+    return path
